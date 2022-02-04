e9c19ae4b1d072c1f1e75a4ef3fa01a024c418d6
Tolga Karasay <tolgakarasay@gmail.com>
2022-02-05 00:47:46 +0300

    update: keep user sessions on database

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
+// MODULES
 import * as express from 'express';
-import 'reflect-metadata';
-import { createConnection } from 'typeorm';
-import { User } from './entity/User';
+import * as cookieParser from 'cookie-parser';
+import * as session from 'express-session';
+import * as ejs from 'ejs';
+import { createConnection, getConnection } from 'typeorm';
+import { TypeormStore } from 'typeorm-store';
+require('dotenv').config();
+import { Session } from './entity/Session';
+import userRoute from './routes/userRoute';
 import pageRoute from './routes/pageRoute';
 
-const app = express();
-
-// ADD GLOBAL VARIABLE
+// ADD GLOBAL VARIABLES
 declare global {
   namespace NodeJS {
     interface Global {
@@ -16,34 +20,46 @@ declare global {
 }
 global.userIN = null;
 
-createConnection()
-  .then(async (connection) => {
-    // MIDDLEWARES
-    app.use(express.static('public'));
+const app = express();
 
-    // TEMPLATE ENGINE
-    app.set('views', './src/views');
-    app.set('view engine', 'ejs');
+(async () => {
+  // DB CONNECTION
+  await createConnection();
 
-    console.log('Inserting a new user into the database...');
-    const user = new User();
-    user.firstName = 'Timber';
-    user.lastName = 'Saw';
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log('Saved a new user with id: ' + user.id);
-    console.log('Loading users from the database...');
-    const users = await connection.manager.find(User);
-    console.log('Loaded users: ', users);
-    console.log('Here you can setup and run express/koa/any other framework.');
+  // MIDDLEWARES
+  app.use(express.static('public'));
+  app.use(express.json({ limit: '50mb' }));
+  app.use(express.urlencoded({ extended: true }));
+  app.use(cookieParser());
 
-    // ROUTES
-    app.use('/', pageRoute);
+  // SESSION
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      store: new TypeormStore({
+        repository: getConnection().getRepository(Session),
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 2, // 2 hours
+        sameSite: true,
+        httpOnly: true,
+      },
+    })
+  );
 
-    // START THE SERVER
-    const port = 3000;
-    app.listen(3000, () => {
-      console.log(`Server started at port ${port}`);
-    });
-  })
-  .catch((error) => console.log(error));
+  // TEMPLATE ENGINE
+  app.set('views', './src/views');
+  app.set('view engine', 'ejs');
+
+  // ROUTES
+  app.use('/', pageRoute);
+  app.use('/', userRoute);
+
+  // START THE SERVER
+  const { API_PORT } = process.env;
+  app.listen(API_PORT, () => {
+    console.log(`Server started at port ${API_PORT}`);
+  });
+})();
