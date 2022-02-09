7d557df9f0c3adf8d36e0658e90c08a4dfa6b652
Tolga Karasay <tolgakarasay@gmail.com>
2022-02-10 02:56:16 +0300

    add actorRoute

---
+++ src/routes/actorRoute.ts
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+import * as actorController from '../controllers/actorController';
+import * as actorLikeController from '../controllers/actorLikeController';
+import * as actorReviewController from '../controllers/actorReviewController';
+const auth = require('../middlewares/authMiddleware').default;
+
+// actor routes
+router.route('/addActor').post(auth, actorController.addActor);
+router.route('/').get(auth, actorController.listAllSharedActors);
+router.route('/:id').delete(auth, actorController.deleteActor);
+router.route('/visibility:id').put(auth, actorController.toggleActorVisibility);
+router.route('/:id').put(auth, actorController.updateActor);
+router.route('/:id').get(auth, actorController.getActor);
+router.route('/like').post(auth, actorLikeController.likeActor);
+router.route('/unlike').post(auth, actorLikeController.unlikeActor);
+router.route('/addReview').post(auth, actorReviewController.addActorReview);
+export default router;
