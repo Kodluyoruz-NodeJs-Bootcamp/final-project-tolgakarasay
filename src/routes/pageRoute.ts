a09e7a8274d50106e5e9183b55ae0ad515884b43
Tolga Karasay <tolgakarasay@gmail.com>
2022-02-09 05:49:19 +0300

    run redirect middleware  before visiting login, signup, and index pages

--- src/routes/pageRoute.ts
+++ src/routes/pageRoute.ts
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 import * as pageController from '../controllers/pageController';
+import { isLoggedIn } from '../middlewares/redirectMiddleware';
 
 // page routes
-router.route('/').get(pageController.getIndexPage);
-router.route('/login').get(pageController.getLoginPage);
-router.route('/signup').get(pageController.getSignupPage);
+router.route('/').get(isLoggedIn, pageController.getIndexPage);
+router.route('/login').get(isLoggedIn, pageController.getLoginPage);
+router.route('/signup').get(isLoggedIn, pageController.getSignupPage);
 
 export default router;
